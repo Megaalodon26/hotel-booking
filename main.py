0aaa8a2b194243beef76bb8fd63c3753aa835dae
meg <megan_lynn@comcast.com>
2025-02-05 16:24:37 -0500

    Deleted main2.py file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas
 
 df = pandas.read_csv("hotels.csv", dtype={"id": str})
 df_cards = pandas.read_csv("cards.csv", dtype=str).to_dict(orient="records")
+df_cards_security = pandas.read_csv("card_security.csv", dtype=str)
 
 class Hotel:
     def __init__(self, hotel_id):
@@ -51,8 +52,12 @@ class CreditCard:
 
 
 class SecureCreditCard(CreditCard):
-    def authenticate(self):
-
+    def authenticate(self, given_password):
+        password = df_cards_security.loc[df_cards_security["number"] == self.number, "password"].squeeze()
+        if password == given_password:
+            return True
+        else:
+            return False
 
 
 print(df)
@@ -60,12 +65,15 @@ hotel_ID = input("Enter the hotel id: ")
 hotel = Hotel(hotel_ID)
 
 if hotel.available():
-    credit_card = CreditCard(number="1234567890123456")
+    credit_card = SecureCreditCard(number="1234567890123456")
     if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
-        hotel.book()
-        name = input("Enter your name: ")
-        reservation_confirmation = ReservationConfirmation(customer_name=name, hotel_object=hotel)
-        print(reservation_confirmation.generate())
+        if credit_card.authenticate(given_password="mypass"):
+            hotel.book()
+            name = input("Enter your name: ")
+            reservation_confirmation = ReservationConfirmation(customer_name=name, hotel_object=hotel)
+            print(reservation_confirmation.generate())
+        else:
+            print("Credit card authentication failed!")
     else:
         print("There was a problem processing your payment")
 else:
