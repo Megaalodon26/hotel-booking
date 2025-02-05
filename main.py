dc691ef2c100553d6238183adf5b2245e4bc8055
meg <megan_lynn@comcast.com>
2025-02-05 15:27:03 -0500

    Initial commit, added classes,instances, methods of hotel, reservation booking, and fake credit card functionality

---
+++ main.py
@@ -0,0 +1,71 @@
+import pandas
+
+df = pandas.read_csv("hotels.csv", dtype={"id": str})
+df_cards = pandas.read_csv("cards.csv", dtype=str).to_dict(orient="records")
+
+class Hotel:
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
+
+    def book(self):
+        """Books a hotel by changing its availability to no"""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
+
+    def available(self):
+        """Checks if the hotel is available"""
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
+
+
+class ReservationConfirmation:
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel = hotel_object
+
+    def generate(self):
+        content = f"""
+        Your reservation is booked!
+        Here's your confirmation details:
+        Name: {self.customer_name}
+        Hotel name: {self.hotel.name}
+        """
+        return content
+
+
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data = {"number": self.number, "expiration": expiration,
+                     "holder": holder, "cvc": cvc}
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
+
+class SecureCreditCard:
+
+
+
+print(df)
+hotel_ID = input("Enter the hotel id: ")
+hotel = Hotel(hotel_ID)
+
+if hotel.available():
+    credit_card = CreditCard(number="1234567890123456")
+    if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
+        hotel.book()
+        name = input("Enter your name: ")
+        reservation_confirmation = ReservationConfirmation(customer_name=name, hotel_object=hotel)
+        print(reservation_confirmation.generate())
+    else:
+        print("There was a problem processing your payment")
+else:
+    print("Hotel is not available")
